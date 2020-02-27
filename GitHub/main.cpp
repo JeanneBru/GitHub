2a5c96474ed2a05b40ef1e0b4a447c1a3321c905
Jeanne Brunard <JeanneBru@users.noreply.github.com>
2020-02-27 11:17:00 +0100

    Initial Commit

---
+++ GitHub/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitHub
+//
+//  Created by Jeanne Alexandra Brunard on 27/02/2020.
+//  Copyright © 2020 Jeanne Alexandra Brunard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
